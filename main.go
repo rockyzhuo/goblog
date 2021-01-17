4387c36d9e1f34ceba13e5a1e62bc95a96dd9e08
rockyzhuo <email@sample.com>
2021-01-17 21:09:17 +0800

    设置标头

--- main.go
+++ main.go
@@ -6,8 +6,18 @@ import (
 )
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "<h1>Hello, 这里是gitgoblog</h1>")
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	if r.URL.Path == "/" {
+		fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
+	} else if r.URL.Path == "/about" {
+		fmt.Fprint(w, "此博客是用以记录编程笔记，如您有反馈或建议，请联系 "+
+			"<a href=\"mailto:summer@example.com\">summer@example.com</a>")
+	} else {
+		fmt.Fprint(w, "<h1>请求页面未找到 :(</h1>"+
+			"<p>如有疑惑，请联系我们。</p>")
+	}
 }
+
 func main() {
 	http.HandleFunc("/", handlerFunc)
 	http.ListenAndServe(":3000", nil)
