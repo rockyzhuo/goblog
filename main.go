c500e149c64123800b41f92bedb690eb159d6f8d
zhuoyanw <644858190@qq.com>
2021-01-28 18:36:05 +0800

    创建数据表

--- main.go
+++ main.go
@@ -136,9 +136,9 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 func initDB() {
 	var err error
 	config := mysql.Config{
-		User:                 "homestead",
-		Passwd:               "secret",
-		Addr:                 "127.0.0.1:33060",
+		User:                 "root",
+		Passwd:               "123456",
+		Addr:                 "127.0.0.1:3305",
 		Net:                  "tcp",
 		DBName:               "goblog",
 		AllowNativePasswords: true,
@@ -166,8 +166,21 @@ func checkError(err error) {
 	}
 }
 
+func createTables() {
+	createArticlesSQL := `CREATE TABLE IF NOT EXISTS articles(
+    id bigint(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    title varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+    body longtext COLLATE utf8mb4_unicode_ci
+); `
+
+	_, err := db.Exec(createArticlesSQL)
+	checkError(err)
+}
+
 func main() {
 	initDB()
+
+	createTables()
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
