3cdcce033b920cc1f7d5f2857d455552de64ed6f
zhuoyanw <644858190@qq.com>
2021-05-08 18:26:24 +0800

    集成 GORM

---
+++ bootstrap/db.go
@@ -0,0 +1,25 @@
+package bootstrap
+
+import (
+	"goblog/pkg/model"
+	"time"
+)
+
+func SetupDB() {
+
+	//建立数据库连接池
+	db := model.ConnectDB()
+
+	// 命令行打印数据库请求信息
+	sqlDB, _ := db.DB()
+
+	// 设置最大连接数
+	sqlDB.SetMaxOpenConns(100)
+
+	// 设置最大空闲连接数
+	sqlDB.SetMaxIdleConns(25)
+
+	// 设置每个链接的过期时间
+	sqlDB.SetConnMaxLifetime(5 * time.Minute)
+
+}
